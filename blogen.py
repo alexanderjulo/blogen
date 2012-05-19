f2645c62f03369e8921d888d99800b949ba9b124
Alexander Jung-Loddenkemper <alexander@julo.ch>
2012-05-19 21:20:59 +0200

    added support for posts and an index for posts.
    to make this possible i had to subclass flat pages, as it did not support multiple instances with different roots.

--- blogen.py
+++ blogen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2.7 
-from flask import Flask, render_template, url_for
+from flask import Flask, Blueprint, render_template, url_for
 from flaskext.themes import ThemeManager, setup_themes, render_theme_template
-from flaskext.flatpages import FlatPages
+from flaskext.flatpages import FlatPages, pygmented_markdown
 from flaskext.script import Manager
 from flask_frozen import Freezer
 
@@ -12,26 +12,47 @@ FLATPAGES_EXTENSION='.md'
 gen = Flask(__name__)
 gen.config.from_object(__name__)
 gen.config.from_pyfile('blogen.cfg')
-setup_themes(gen, app_identifier='blogen')
-pages = FlatPages(gen)
+setup_themes(gen, app_identifier='blogen', theme_url_prefix='/theme')
+class FlexFlatPages(FlatPages):
+	def __init__(self, app, root):
+		app.config.setdefault('FLATPAGES_ROOT', 'pages')
+		app.config.setdefault('FLATPAGES_EXTENSION', '.html')
+		app.config.setdefault('FLATPAGES_ENCODING', 'utf8')
+		app.config.setdefault('FLATPAGES_HTML_RENDERER', pygmented_markdown)
+		app.config.setdefault('FLATPAGES_AUTO_RELOAD', 'if debug')
+		self.app = app
+		self.root = root
+		
+		#: dict of filename: (page object, mtime when loaded) 
+		self._file_cache = {}
+		
+		app.before_request(self._conditional_auto_reset)
+		
+	def root(self):
+		return os.path.join(self.app.root_path, self.root)
+		
+FLATPAGES_ROOT='pages'
+pages = FlexFlatPages(gen, 'pages')
+FLATPAGES_ROOT='posts'
+posts = FlexFlatPages(gen, 'posts')
 cli = Manager(gen)
 static = Freezer(gen)
 
 @gen.route('/')
 def index():
-	pass
+	return "Bla"
 	
 @gen.route('/<page>/')
 def page(page):
-	return render_theme_template(gen.config['THEME'], 'page.html', content=pages.get_or_404(page).html)
+	return render_theme_template(gen.config['THEME'], 'page.html', page=pages.get_or_404(page))
 		
 @gen.route('/blog/')
-def posts():
-	pass
+def postindex():
+	return render_theme_template(gen.config['THEME'], 'index.html', list=posts)
 	
 @gen.route('/blog/<post>/')
 def post(post):
-	pass
+	return render_theme_template(gen.config['THEME'], 'post.html', post=posts.get_or_404(post))
 	
 # inject some standard vars into templates
 @gen.context_processor
@@ -45,6 +66,13 @@ def inject_menu():
 	for page in pages:
 		menu.append((page.meta['title'], url_for("page", page=page.path)))
 	return dict(menu=menu)
+	
+# make sure all urls are found
+@static.register_generator
+def page():
+	for page in pages:
+		yield {'page': page.path}
+
 	
 # cli-interface
 @cli.command
